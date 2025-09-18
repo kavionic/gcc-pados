88c96f6874d7f2b992d5be17561424799ac79434
kavionic <kurt.skauen@gmail.com>
2025-09-19 01:07:08 +0200

    Moved PadOS port from 13.3.0 to 14.2.0.

---
+++ gcc/config/arm/pados.h
@@ -0,0 +1,28 @@
+/*
+ * Copyright (C) 2025 Kurt Skauen. All rights reserved.
+ *
+ * SPDX-License-Identifier: Apache-2.0
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     https://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+#define HAS_INIT_SECTION
+
+#undef TARGET_OS_CPP_BUILTINS
+#define TARGET_OS_CPP_BUILTINS()		\
+    do {					\
+	builtin_define ("__pados__");		\
+	builtin_assert ("system=pados");	\
+	TARGET_BPABI_CPP_BUILTINS();    	\
+    } while (0)
+
--- libstdc++-v3/acinclude.m4
+++ libstdc++-v3/acinclude.m4
@@ -1792,7 +1792,7 @@ AC_DEFUN([GLIBCXX_ENABLE_LIBSTDCXX_TIME], [
         ac_has_nanosleep=yes
         ac_has_sched_yield=yes
         ;;
-      freebsd*|netbsd*|dragonfly*|rtems*)
+      freebsd*|netbsd*|dragonfly*|rtems*|pados*)
         ac_has_clock_monotonic=yes
         ac_has_clock_realtime=yes
         ac_has_nanosleep=yes
@@ -4940,6 +4940,9 @@ AC_DEFUN([GLIBCXX_ENABLE_FILESYSTEM_TS], [
         enable_libstdcxx_filesystem_ts=yes
         ;;
       gnu* | linux* | kfreebsd*-gnu | knetbsd*-gnu | uclinux*)
+        enable_libstdcxx_filesystem_ts=yes
+        ;;
+      pados*)
         enable_libstdcxx_filesystem_ts=yes
         ;;
       rtems*)
